325be535b30a6d0d76288083e07c258f5ccf52fe
Marissa Rousseau <marissa@rousseau.page>
2020-05-24 01:05:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# politicalspectrumquiz
+ A political spetrum quiz that determines which quadrant of the political spectrum you belong in.
---
+++ main.js
@@ -0,0 +1,64 @@
+function calculate()
+{
+  var lefts = document.getElementsByClassName("left");
+  var rights = document.getElementsByClassName("right");
+
+  var leftTotal = 0;
+  var rightTotal = 0;
+
+  for(var x=0; x<lefts.length; x++)
+  {
+    leftTotal += Number(lefts[x].value);
+  }
+
+  for(var x=0; x<rights.length; x++)
+  {
+    rightTotal += Number(rights[x].value);
+  }
+
+  var leftRightTotal = rightTotal + leftTotal;
+
+  if(leftRightTotal < 0)
+  {
+    sessionStorage.setItem("leftRight","Left-wing")
+  }
+  else
+  {
+    sessionStorage.setItem("leftRight","Right-wing")
+  }
+
+  var ups = document.getElementsByClassName("up");
+  var downs = document.getElementsByClassName("down");
+
+  var upTotal = 0;
+  var downTotal = 0;
+
+  for(var x=0; x<ups.length; x++)
+  {
+    upTotal += Number(ups[x].value);
+  }
+
+  for(var x=0; x<downs.length; x++)
+  {
+    downTotal += Number(downs[x].value);
+  }
+
+  var upDownTotal = upTotal + downTotal;
+
+  if(upDownTotal < 0)
+  {
+    sessionStorage.setItem("upDown","Libertarian")
+  }
+  else
+  {
+    sessionStorage.setItem("upDown","Authoritarian")
+  }
+
+  window.location.href = "results.html";
+}
+
+function displayResult()
+{
+  var result = document.getElementById("result");
+  result.innerHTML = "Your result is: " + sessionStorage.getItem("leftRight") + " " + sessionStorage.getItem("upDown");
+}
